3ecdc95d31deb8e261f3c816c8bfdfce3b11ab86
yaserananbeh <yaser.ananbeh74@gmail.com>
2021-12-01 12:37:16 +0200

    test push

---
+++ src/components/HeroImage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class HeroImage extends Component {
+    render() {
+        return (
+            <div>
+                <h1>test my component</h1>
+            </div>
+        )
+    }
+}
+
+export default HeroImage
