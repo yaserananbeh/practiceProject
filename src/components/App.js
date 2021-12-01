d9c3fe40d7ad294914f6e651e2903ce92e25e3c9
yaserananbeh <yaser.ananbeh74@gmail.com>
2021-12-01 17:32:04 +0200

    merge haneen work

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import HeroImage from "./HeroImage"
+import Footer from "./Footer"
 
 export class App extends Component {
   render() {
     return (
       <div>
         <HeroImage/>
+        <Footer/>
       </div>
     )
   }
